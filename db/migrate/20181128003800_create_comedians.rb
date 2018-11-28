9ab6188a9f8cdb841716bf732b80aa98228fb81d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-27 17:46:28 -0700

    Adds create_table in migration for comedians

--- db/migrate/20181128003800_create_comedians.rb
+++ db/migrate/20181128003800_create_comedians.rb
@@ -1,4 +1,11 @@
 class CreateComedians < ActiveRecord::Migration[5.2]
   def change
+    # why is the below called t?
+    create_table :comedians do |t|
+      t.text    :name
+      t.integer :age
+      t.text    :city
+      t.timestamps null: false
+    end
   end
 end
