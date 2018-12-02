5d89aa40a9f2782607ba29b0f64f66de3eeb058f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-01 17:02:51 -0700

    Tests: When querying for a specific comedian age, only comedians matching that age show up. Integrates: Coditionals in the controller to accomplish such.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,11 +2,13 @@ class LaughTracksApp < Sinatra::Base
   # what does the below do?
   set :root, File.expand_path("..", __dir__)
 
-  get ('/comedians') do
-    @comedians = Comedian.all
+  get '/comedians' do
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+      @specials = Special.where(comedian_id: @comedians.ids)
+    else
+      @comedians = Comedian.all
+    end
     erb :'comedians/index'
   end
-
-
-
 end
